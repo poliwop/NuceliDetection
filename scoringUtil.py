95d3e34fc08fce5b40a52f0f9efe835b2bf3e454
Colin Grove <grove.colin@gmail.com>
2018-01-28 17:58:15 -0600

    removed optimization that did not work'

--- scoringUtil.py
+++ scoringUtil.py
@@ -22,40 +22,6 @@ def get_feature_mask(output_str, image_size):
             feature_mask[top_pixel_r + j][top_pixel_c] = 1
     return feature_mask
 
-def get_rect_coords(output_str, image_size):
-    h = image_size[0]
-    output_list = output_str.split()
-    output_list = np.array(output_list, np.uint16)
-    top_pixels = output_list[::2] - 1
-    bottom_pixels = top_pixels + output_list[1::2] - 1
-    left_col = top_pixels[0] // h
-    right_col = top_pixels[-1] // h
-    top_pixels_r = top_pixels % h
-    top_row = min(top_pixels_r)
-    bottom_pixels_r = bottom_pixels % h
-    bottom_row = max(bottom_pixels_r)
-    return (top_row, left_col, bottom_row, right_col)
-
-def do_rects_intersect(rect_1, rect_2):
-    intersect = False
-    (x1, y1, w1, z1) = rect_1
-    (x2, y2, w2, z2) = rect_2
-    if x1 <= w2 and y1 <= z2 and w1 >= x2 and z1 >= y2:
-        intersect = True
-    return intersect
-
-def test_do_rects_intersect():
-    rect1 = (1, 1, 5, 6)
-    rect2 = (3, 3, 7, 6)
-    rect3 = (10, 1, 12, 2)
-    rect4 = (1, 10, 1, 10)
-    print(do_rects_intersect(rect1, rect2))
-    print(do_rects_intersect(rect2, rect1))
-    print(do_rects_intersect(rect1, rect3))
-    print(do_rects_intersect(rect3, rect1))
-    print(do_rects_intersect(rect1, rect4))
-    print(do_rects_intersect(rect4, rect1))
-
 
 def get_image_prediction(features, image_size):
     image = np.zeros(image_size[:2], np.uint16)
@@ -63,35 +29,6 @@ def get_image_prediction(features, image_size):
         feature_mask = get_feature_mask(feature_str, image_size)
         image[feature_mask > 0] = i+1
     return image
-
-def get_rect_intersections(image_data, key_data, image_size):
-    image_rect_list = [None]*len(image_data)
-    key_rect_list = [None]*len(key_data)
-    for i,feature in enumerate(image_data):
-        image_rect_list[i] = get_rect_coords(feature, image_size) + (i + 1,)
-    for i,feature in enumerate(key_data):
-        key_rect_list[i] = get_rect_coords(feature, image_size) + (i + 1,)
-    image_rect_list.sort()
-    key_rect_list.sort()
-
-    intersecting_pairs = []
-    min_j = 0
-    i = 0
-    while i < len(image_rect_list):
-        image_rect = image_rect_list[i]
-        while min_j < len(key_rect_list) and image_rect[0] > key_rect_list[min_j][2]:
-            min_j += 1
-        j = min_j
-        while j < len(key_rect_list) and image_rect[2] > key_rect_list[j][0]:
-            if do_rects_intersect(image_rect[:4], key_rect_list[j][:4]):
-                intersecting_pairs.append((i,j))
-            j += 1
-        i += 1
-
-    pairs = [None]*len(intersecting_pairs)
-    for j,(image_i, key_i) in enumerate(intersecting_pairs):
-        pairs[j] = (image_rect_list[image_i][4], key_rect_list[key_i][4])
-    return pairs
 
 # Precision helper function
 def precision_at(threshold, iou):
@@ -113,31 +50,8 @@ def get_score(image_data, key_data, image_size):
     print("Number of true objects:", true_objects)
     print("Number of predicted objects:", pred_objects)
 
-    '''
+
     # Compute intersection between all objects
-    rect_intersections = get_rect_intersections(image_data, key_data, image_size)
-    intersection = np.zeros([len(key_data) + 1, len(image_data) + 1])
-    for (pred_feature_id, true_feature_id) in rect_intersections:
-        pred_mask = np.zeros_like(y_pred)
-        pred_mask[y_pred == pred_feature_id] = pred_feature_id
-        true_mask = np.zeros_like(labels)
-        true_mask[labels == true_feature_id] = true_feature_id
-        temp = np.histogram2d(true_mask.flatten(), pred_mask.flatten(), bins=(2, 2))[0][1][1]
-        intersection[true_feature_id, pred_feature_id] = temp
-
-
-
-    for i in range(len(key_data)):
-        mask = np.zeros_like(labels)
-        mask[labels == i+1] = 1
-        intersection[i + 1][0] = np.sum(mask, axis=(0, 1)) - np.sum(intersection, axis = 1)[i + 1]
-    for i in range(len(image_data)):
-        mask = np.zeros_like(y_pred)
-        mask[y_pred == i+1] = 1
-        intersection[0][i + 1] = np.sum(mask, axis=(0, 1)) - np.sum(intersection, axis = 0)[i + 1]
-
-    intersection[0][0] = image_size[0] * image_size[1] - np.sum(intersection)
-    '''
     intersection = np.histogram2d(labels.flatten(), y_pred.flatten(), bins=(true_objects, pred_objects))[0]
 
     # Compute areas (needed for finding the union between all objects)
