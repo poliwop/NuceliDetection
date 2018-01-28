6d4162979885cc4b0c33adbcf0a40b49bc3175ae
Colin Grove <grove.colin@gmail.com>
2018-01-27 18:30:17 -0600

    Added image names to object returned by get_image_list

--- nucleiUtil.py
+++ nucleiUtil.py
@@ -3,12 +3,12 @@ from os import listdir
 import numpy as np
 
 def get_image_list(dir):
-    image_list = [0]*len(listdir(dir))
+    image_list = [None]*len(listdir(dir))
 
     print "converting images to matrices"
     for i in range(len(listdir(dir))):
-        folder = listdir(dir)[i]
-        img_path = dir + folder + '/images/' + folder + '.png'
+        image_id = listdir(dir)[i]
+        img_path = dir + image_id + '/images/' + image_id + '.png'
         img = Image.open(img_path).convert('L')
-        image_list[i] = np.array(img)
+        image_list[i] = [image_id, np.array(img)]
     return image_list
