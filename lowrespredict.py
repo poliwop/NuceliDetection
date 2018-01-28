5ce19fddb8e06593eaf9a2a3adf1298d23b1d3ab
Colin Grove <grove.colin@gmail.com>
2018-01-27 23:38:02 -0600

    Replaced while loops with for loops

--- lowrespredict.py
+++ lowrespredict.py
@@ -2,10 +2,12 @@ from nucleiUtil import get_image_list
 import csv
 import numpy as np
 
-testing_data_path = 'data folder/stage1_test/'
+testing_data_path = 'data/stage1_test/'
+output_filename = 'prediction.csv'
 image_list = get_image_list(testing_data_path)
-threshhold = 200 #keep pixels at least this bright
+threshold = 200 #keep pixels at least this bright
 factor = 16 #compress width/height by factor of 16
+intensity_threshold = 0.3
 
 def lower_resolution(array, factor):
     input_columns = array.shape[1]
@@ -15,27 +17,19 @@ def lower_resolution(array, factor):
     output = np.zeros(shape=(output_rows, output_columns))
     if (input_rows % factor) or (input_columns % factor):
         return output
-    i=0
-    while i < output_rows:
-        j=0
-        while j < output_columns:
+    for i in range(output_rows):
+        for j in range(output_columns):
             pixel = 0
-            k=0
-            while k < factor:
-                l=0
-                while l < factor:
+            for k in range(factor):
+                for l in range(factor):
                     pixel += array[i*factor+k][j*factor+l]
-                    l += 1
-                k += 1
             output[i][j]=float(pixel)/(factor*factor)
-            j += 1
-        i += 1
-    if np.average(output)/255>=0.3:
+    if np.average(output)/255 >= intensity_threshold:
         output = 255-output
     output *= (255.0/output.max())
     return output
 
-with open('prediction.csv', 'w') as outputfile:
+with open(output_filename, 'w') as outputfile:
     writer = csv.writer(outputfile, lineterminator='\n')
     writer.writerow(['ImageId', 'EncodedPixels'])
     counter=0
@@ -43,13 +37,11 @@ with open('prediction.csv', 'w') as outputfile:
         test = image[1].shape[0]*image[1].shape[1]
         height =image[1].shape[0] #height of original image in pixels
         pixelated = lower_resolution(image[1], factor)
-        i=0
         if pixelated.max() == 0:
             writer.writerow([image[0], '1 1'])
-        while i < pixelated.shape[0]:
-            j=0
-            while j < pixelated.shape[1]:
-                if pixelated[i][j] > threshhold:
+        for i in range(pixelated.shape[0]):
+            for j in range(pixelated.shape[1]):
+                if pixelated[i][j] > threshold:
                     pixels = ''
                     for k in range(factor):
                         pixels += str(j*factor*height+i*factor+k*height+1)+' 8 '
@@ -60,8 +52,6 @@ with open('prediction.csv', 'w') as outputfile:
                             print i, j, pixelated[i][j]
                             raise
                     writer.writerow([image[0], pixels])
-                j+=1
-            i+=1
         counter += 1
         if counter % 10 == 0:
-            print 'starting image number'+str(counter)
+            print 'starting image number '+str(counter)
