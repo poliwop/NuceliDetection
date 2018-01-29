38b154f89f33a4190e2ccfa077d82e8a0d61db8f
Colin Grove <grove.colin@gmail.com>
2018-01-28 22:07:51 -0600

    put stuff in a function for easier testing

--- lowrespredict.py
+++ lowrespredict.py
@@ -2,14 +2,8 @@ from nucleiUtil import get_image_list
 import csv
 import numpy as np
 
-testing_data_path = 'data/stage1_test/'
-output_filename = 'prediction.csv'
-image_list = get_image_list(testing_data_path)
-threshold = 150 #keep pixels at least this bright
-factor = 16 #compress width/height by factor of 16
-intensity_threshold = 0.3
-
 def lower_resolution(array, factor):
+    intensity_threshold = 0.3
     input_columns = array.shape[1]
     input_rows = array.shape[0]
     output_columns = input_columns//factor
@@ -27,28 +21,37 @@ def lower_resolution(array, factor):
     output *= (255.0/output.max())
     return output
 
-with open(output_filename, 'w') as outputfile:
-    writer = csv.writer(outputfile, lineterminator='\n')
-    writer.writerow(['ImageId', 'EncodedPixels'])
-    for counter,image in enumerate(image_list):
-        test = image[1].shape[0]*image[1].shape[1]
-        height =image[1].shape[0] #height of original image in pixels
-        pixelated = lower_resolution(image[1], factor)
-        if pixelated.max() == 0:
-            writer.writerow([image[0], '1 1'])
-        for i in range(pixelated.shape[0]):
-            for j in range(pixelated.shape[1]):
-                if pixelated[i][j] > threshold:
-                    pixels = ''
-                    for k in range(factor):
-                        top_pixel = (j*factor + k)*height + i*factor + 1
-                        pixels += str(top_pixel)+' ' + str(factor) + ' '
-                        if top_pixel > test:
-                            print(image[0])
-                            print(test)
-                            print(top_pixel)
-                            print([i, j, pixelated[i][j]])
-                            raise
-                    writer.writerow([image[0], pixels])
-        if counter % 10 == 0:
-            print('starting image number '+str(counter))
+def run_lowrespredict(output_filename, image_list, factor, threshold):
+    with open(output_filename, 'w') as outputfile:
+        writer = csv.writer(outputfile, lineterminator='\n')
+        writer.writerow(['ImageId', 'EncodedPixels'])
+        for counter,image in enumerate(image_list):
+            test = image[1].shape[0]*image[1].shape[1]
+            height =image[1].shape[0] #height of original image in pixels
+            pixelated = lower_resolution(image[1], factor)
+            if pixelated.max() == 0:
+                writer.writerow([image[0], '1 1'])
+            for i in range(pixelated.shape[0]):
+                for j in range(pixelated.shape[1]):
+                    if pixelated[i][j] > threshold:
+                        pixels = ''
+                        for k in range(factor):
+                            top_pixel = (j*factor + k)*height + i*factor + 1
+                            pixels += str(top_pixel)+' ' + str(factor) + ' '
+                            if top_pixel > test:
+                                print(image[0])
+                                print(test)
+                                print(top_pixel)
+                                print([i, j, pixelated[i][j]])
+                                raise
+                        writer.writerow([image[0], pixels])
+            if counter % 10 == 0:
+                print('starting image number '+str(counter))
+
+
+testing_data_path = 'data/stage1_test/'
+output_filename = 'prediction.csv'
+image_list = get_image_list(testing_data_path)
+threshold = 120 #keep pixels at least this bright
+factor = 14 #compress width/height by factor of 16
+run_lowrespredict(output_filename, image_list, factor, threshold)
