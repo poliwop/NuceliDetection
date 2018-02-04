890284df97e25984b95c40b9abce4de6641cf3ad
Nicole <nbridgland@gmail.com>
2018-02-03 21:07:19 -0500

    now writes score/image csv

--- scoring.py
+++ scoring.py
@@ -1,5 +1,6 @@
 from scoringUtil import score_output
 import cProfile
+import csv
 
 training_data_path = 'data/stage1_train/'
 output_csv = 'predictionBasic.csv'
@@ -7,3 +8,9 @@ key_csv = 'data/stage1_train_labels.csv'
 
 myscore = score_output(output_csv, key_csv, training_data_path)
 print(myscore[0])
+
+with open('scoreperimage.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file, lineterminator='\n')
+    writer.writerow(["ImageID", "Score"])
+    for key, value in myscore[1].items():
+        writer.writerow([str(key), str(value)])
